89f2580377d7a647d84a8694430694b3b29bc9ac
perawita <perawitayasa@gmail.com>
2024-08-31 01:59:49 +0800

    update

--- api/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * file suprt ini di buat hanya untuk mendukung proses deploy di vercel 
- */
-require __DIR__ . 'public/index.php';
-
-phpinfo();
--- public/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * file suprt ini di buat hanya untuk mendukung proses deploy di vercel 
- */
-require __DIR__ . 'index.php';
